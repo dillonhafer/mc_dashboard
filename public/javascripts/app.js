7f23dd00d18909904dca410d8185fa618627f17e
Dillon Hafer <dh@dillonhafer.com>
2014-10-13 13:32:34 -0400

    Updates front end styles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,6 @@
-$(document).on('click', '.button', function(e) {
+$(document).on('click', '.f-dropdown a', function(e) {
   e.preventDefault();
+  $(this).parent().parent().css('left', '-9999999px')
   $.post(this.getAttribute('href'));
 })
 $(document).foundation();
